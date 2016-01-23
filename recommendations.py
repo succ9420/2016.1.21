d476bff5f619a6d810ab6b870b35a70491e21a93
myname <succ9420@gmail.com>
2016-01-23 21:11:31 +0800

    git tracks changes

--- recommendations.py
+++ recommendations.py
@@ -22,14 +22,31 @@ def sim_distance(prefs,person1,person2):
 
 	return 1/(1+sqrt(sum_of_squares))
 
+def topMatches(prefs,person,n=5,similarity=sim_distance):
+	scores = [(similarity(prefs,person,other),other) for other in prefs if other !=person]
+	scores.sort()
+	scores.reverse()
+	return scores[0:n]
+
+def getRecommendations(prefs,person,similarity = sim_distance):
+	totals = {}
+	simSums = {}
+	for other in prefs:
+		if other == person:
+			continue
+		sim = similarity(prefs,person,other)
+		if sim <= 0 :
+			continue
+		for item in prefs[other]:
+			if item not in prefs[person] or prefs[person][item] == 0:
+				totals.setdefault(item,0)
+				totals[item] += prefs[other][item] * sim
+				simSums.setdefault(item,0)	
+				simSums[item] += sim
+
+		rankings = [(total/simSums[item],item) for item,total in totals.items()]
+		rankings.sort()
+		rankings.reverse()
+		return rankings
 
 
-
-#d = sim_distance (critics,'Lisa Rose','Gene Seymour')
-
-#print d
-
-
-
-
-# 2016.1.21
